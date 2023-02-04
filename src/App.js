140ca15724df12c4d988ebbdca3f21f3f34b76f1
arrowls <babuyoff@gmail.com>
2023-02-04 20:09:50 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import postsItems from "./static/posts";
 
 export default function App() {
     const [filter, updateFilter] = useState( 'all');
-    const [posts, setPosts] = useState({sorted: [...postsItems]});
+    const [posts, setPosts] = useState(postsItems);
 
     const filters = ['all', 'Websites', 'Flayers', 'Business Cards'];
 
@@ -18,33 +18,20 @@ export default function App() {
 
     function filterPosts() {
         if (filter === 'all') {
-            setPosts({ sorted: postsItems} );
+            setPosts(postsItems);
         } else {
             const filtered = postsItems.filter((post) => post.category === filter);
-            setPosts({ sorted: filtered });
+            setPosts(filtered);
         }
-    }
-
-    function spreadPosts(col) {
-        return posts.sorted.filter((item, index) => !((index + col) % 3));
     }
 
     return (
         <>
             <Filters selected={filter} handler={(s) => changeFilter(s)} items={filters}/>
             <div className="s-posts__container">
-                <div className="s-posts__column">
-                    {spreadPosts(1).map((post, index) =>
-                        <img key={index} src={post.img} alt=""/>)}
-                </div>
-                <div className="s-posts__column">
-                    {spreadPosts(2).map((post, index) =>
-                        <img key={index} src={post.img} alt=""/>)}
-                </div>
-                <div className="s-posts__column">
-                    {spreadPosts(3).map((post, index) =>
-                        <img key={index} src={post.img} alt=""/>)}
-                </div>
+                {posts.map((post, id) =>
+                    <img src={post.img} alt=" " className="s-posts__img" key={id}/>
+                )}
             </div>
         </>
     )
