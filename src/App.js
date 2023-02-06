77bf80b7149b652f5734d6a424a656a1a4b5cd7a
arrowls <babuyoff@gmail.com>
2023-02-06 10:29:29 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ export default function App() {
 
     const filters = ['all', 'Websites', 'Flayers', 'Business Cards'];
 
-    function changeFilter(newFilter) {
-        updateFilter(newFilter);
-    }
-
     useEffect(() => {
         filterPosts();
     }, [filter]);
@@ -27,7 +23,7 @@ export default function App() {
 
     return (
         <>
-            <Filters selected={filter} handler={(s) => changeFilter(s)} items={filters}/>
+            <Filters selected={filter} handler={(s) => updateFilter(s)} items={filters}/>
             <div className="s-posts__container">
                 {posts.map((post, id) =>
                     <img src={post.img} alt=" " className="s-posts__img" key={id}/>
